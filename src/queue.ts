5a5b87db2f7e367899dadde56594cc183ef3010b
Evan Kysley <evan@kysley.com>
2021-06-29 00:01:30 -0400

    job seeding & queue

--- src/queue.ts
+++ src/queue.ts
@@ -1,31 +1,34 @@
 import Queue from "bull";
-import got from "got";
-import { URL } from "url";
-import { creds, redis } from "./redis";
-import { prisma } from "./utils";
+
+import { creds, setCollectionWallpaper } from "./redis";
+import { prisma, waitToGetUnsplashUrl } from "./utils";
 
 const pictureQueue = new Queue("Picture", { redis: { ...creds } });
 
 pictureQueue.process(async (job) => {
   const {
-    data: { collection },
+    data: { collectionId },
   } = job;
 
-  const res = await got("https://source.unsplash.com/random/1920x1080");
-  // this is the url for the image
-  // default quality is 80%, we want 100%
-  const url = new URL(res.url);
-  url.searchParams.set("q", "100");
+  const url = await waitToGetUnsplashUrl();
 
-  await redis.set(collection, url.href);
-  // await prisma.wallpaper.create({
-  //   data: {
-  //     u_url: url.href,
-  //     collection,
-  //   },
-  // });
+  await setCollectionWallpaper(collectionId, url);
+  await prisma.wallpaper.create({
+    data: {
+      u_url: url,
+      collection: {
+        connect: {
+          id: collectionId,
+        },
+      },
+    },
+  });
 });
 
-pictureQueue.add({ collection: "RANDOM" });
-// pictureQueue.add({ collection });
+// pictureQueue.add({ collectionId: Math.floor(Math.random() * 3 + 1) });
+// pictureQueue.add({ collectionId: Math.floor(Math.random() * 3 + 1) });
+// pictureQueue.add({ collectionId: Math.floor(Math.random() * 3 + 1) });
+// pictureQueue.add({ collectionId: 1 });
+// pictureQueue.add({ collectionId: 2 });
+// pictureQueue.add({ collectionId: 3 });
 pictureQueue.on("failed", console.log);
